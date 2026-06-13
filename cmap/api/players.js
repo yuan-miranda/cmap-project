388011d4487af5729691b86fa14854373574cb7b
yuan-miranda <yuanezekielamiranda@gmail.com>
2026-06-13 15:22:50 +0800

    Update player data fetching URL to use the correct endpoint for players.json

--- cmap/api/players.js
+++ cmap/api/players.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-    const targetUrl = `http://143.244.173.238:5000/api/players`;
+    const targetUrl = `http://143.244.173.238:5000/tiles/players.json`;
 
     try {
         const response = await fetch(targetUrl, {
